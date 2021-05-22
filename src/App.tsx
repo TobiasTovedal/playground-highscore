130428f2f2bab88178e02ff2a9d18c4c517496a9
tobias <tobias.tovedal@gmail.com>
2021-05-22 20:06:00 +0200

    Setup with tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-br from-light-blue-400 to-indigo-500">
+      <p>HIGHSCORE</p>
+      <p>
+        1. Namn Namnsson, 2021-05-22, 7p <br />
+        2. Namn Namnsson, 2021-05-22, 7p <br />
+        3. Namn Namnsson, 2021-05-22, 7p <br />
+        4. Namn Namnsson, 2021-05-22, 7p <br />
+        5. Namn Namnsson, 2021-05-22, 7p <br />
+        6. Namn Namnsson, 2021-05-22, 7p <br />
+        7. Namn Namnsson, 2021-05-22, 7p <br />
+        8. Namn Namnsson, 2021-05-22, 7p <br />
+        9. Namn Namnsson, 2021-05-22, 7p <br />
+        10. Namn Namnsson, 2021-05-22, 7p
+      </p>
+      <p>NYTT RESULTAT</p>
+      <input defaultValue="NAMN"></input>
+
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
