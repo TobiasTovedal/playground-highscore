1c0ce5ce3927a0bfe8a109d56cb9ef707c9db32d
tobias <tobias.tovedal@gmail.com>
2021-05-26 20:50:35 +0200

    Change PointSelector to styled version

--- src/PointSelectorComponent.tsx
+++ src/PointSelectorComponent.tsx
@@ -1,49 +1,75 @@
-import { useState } from "react"
+import { Fragment, useState } from "react"
 import { Listbox, Transition } from "@headlessui/react"
+import { CheckIcon, SelectorIcon } from "@heroicons/react/solid"
 
 const people = [
-  { id: 1, name: "Durward Reynolds", unavailable: false },
-  { id: 2, name: "Kenton Towne", unavailable: false },
-  { id: 3, name: "Therese Wunsch", unavailable: false },
-  { id: 4, name: "Benedict Kessler", unavailable: true },
-  { id: 5, name: "Katelyn Rohan", unavailable: false },
+  { name: "Wade Cooper" },
+  { name: "Arlene Mccoy" },
+  { name: "Devon Webb" },
+  { name: "Tom Cook" },
+  { name: "Tanya Fox" },
+  { name: "Hellen Schmidt" },
 ]
 
-function PointSelector() {
-  const [selectedPerson, setSelectedPerson] = useState(people[0])
+export default function PointSelector() {
+  const [selected, setSelected] = useState(people[0])
 
   return (
-    <Listbox value={selectedPerson} onChange={setSelectedPerson}>
-      {({ open }) => (
-        <>
-          <Listbox.Button>{selectedPerson.name}</Listbox.Button>
-          {/*
-            Use the Transition + open render prop argument to add transitions.
-          */}
+    <div className="w-72 fixed top-16">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+            <span className="block truncate">{selected.name}</span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <SelectorIcon
+                className="w-5 h-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
           <Transition
-            show={open}
-            enter="transition duration-100 ease-out"
-            enterFrom="transform scale-95 opacity-0"
-            enterTo="transform scale-100 opacity-100"
-            leave="transition duration-75 ease-out"
-            leaveFrom="transform scale-100 opacity-100"
-            leaveTo="transform scale-95 opacity-0"
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            {/*
-              Don't forget to add `static` to your Listbox.Options!
-            */}
-            <Listbox.Options static>
-              {people.map((person) => (
-                <Listbox.Option key={person.id} value={person}>
-                  {person.name}
+            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {people.map((person, personIdx) => (
+                <Listbox.Option
+                  key={personIdx}
+                  className={({ active }) =>
+                    `${active ? "text-amber-900 bg-amber-100" : "text-gray-900"}
+                          cursor-default select-none relative py-2 pl-10 pr-4`
+                  }
+                  value={person}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`${
+                          selected ? "font-medium" : "font-normal"
+                        } block truncate`}
+                      >
+                        {person.name}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`${
+                            active ? "text-amber-600" : "text-amber-600"
+                          }
+                                absolute inset-y-0 left-0 flex items-center pl-3`}
+                        >
+                          <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
                 </Listbox.Option>
               ))}
             </Listbox.Options>
           </Transition>
-        </>
-      )}
-    </Listbox>
+        </div>
+      </Listbox>
+    </div>
   )
 }
-
-export default PointSelector
