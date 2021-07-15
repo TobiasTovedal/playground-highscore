e22f25129d90658156385c6e60a17b50658ce561
tobias <tobias.tovedal@gmail.com>
2021-07-15 15:55:59 +0200

    Fix init of Player array

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,7 @@ import { firebaseConfig } from "./firebaseConfig"
 import { useCollectionData } from "react-firebase-hooks/firestore"
 
 function App() {
-  let players: Player[] = [
-    {
-      name: "",
-      date: "",
-      score: 0
-    }
-  ]
+  let players: Player[] = []
   const possibleScores: number[] = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
   ]
